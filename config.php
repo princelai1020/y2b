d6b37b6993fa6fee978b01a69340ef03054be6d9
princelai1020 <71810945+princelai1020@users.noreply.github.com>
2020-09-24 15:35:50 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,11 +1,11 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', 'AIzaSyCVkRcSiF0KmLCwmoo4edtw0vnSykCvFcc');
+define('APIKEY', 'AIzaSyCgKqzhhNElfO9j9Vh_We4gU5mkbsqf8SQ');
 define('GJ_CODE', 'US');
 define('SITE_NAME', 'y2b');
 define('TITLENAME', 'y2b');
 define('EN2DEKEY', '32123wwerwer323tgfdgds');
-define('EMAIL', '1334347212@qq.com');
+define('EMAIL', 'poting1128@gmail.com');
 define('NAME', 'admin');
 define('PASSWORD', 'admin');
 define('LOGINHTML', '<!DOCTYPE html>
